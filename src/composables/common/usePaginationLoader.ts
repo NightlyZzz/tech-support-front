31be5939feca2fb10941ed558aa34bb0d188f175
Maxim Astaoglo <70810340+NightlyZzz@users.noreply.github.com>
2026-04-03 12:23:52 +0300

    Update

--- src/composables/common/usePaginationLoader.ts
+++ src/composables/common/usePaginationLoader.ts
@@ -1,9 +1,16 @@
 import type { Ref } from 'vue'
 
+interface PaginationMeta {
+    current_page: number
+    last_page: number
+    per_page: number
+    total: number
+}
+
 export const usePaginationLoader = (
         currentPage: Ref<number>,
-        load: (page: number, setMeta: (meta: any) => void) => Promise<void>,
-        setMeta: (meta: any) => void
+        load: (page: number, setMeta: (meta: PaginationMeta) => void) => Promise<void>,
+        setMeta: (meta: PaginationMeta) => void
 ) => {
     const loadPage = async (page: number) => {
         currentPage.value = page
